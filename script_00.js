8bf56ab32e7ba77e4e0fc19b15c1146124235ec2
Jakub Kwiatkowski <jakub1kwiatkowski@gmail.com>
2018-02-20 17:15:04 +0100

    up to 22

--- script_00.js
+++ script_00.js
@@ -4,11 +4,40 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Hello world!'
+        name: 'Jake',
+        title: 'Hello world!',
+        link: 'http://google.com',
+        finishedLink: '<a href="http://google.com">Google</a>',
+        counter: 0,
+        x: 0,
+        y: 0
     },
     methods : {
         changeTitle: function(event) {
             this.title = event.target.value;
+        },
+        sayHello: function() {
+            this.title = 'Say Hello changed by sayHello function';
+            return 'Hey Joe say Hello!';
+        },
+        sayTitle: function() {
+            return 'the data.title from method: '+this.title;
+        },
+        increase: function(step, event) { // noneed 'event' here
+            this.counter += step;
+        },
+        updateCoordinates: function(event) {
+            this.x = event.clientX;
+            this.y = event.clientY;
+        }
+        ,
+        // dummy: function(event) {
+        //     event.stopPropagation();
+        // }
+        alertMe: function () {
+            alert('Alerto moi!');
         }
     }
 });
+
+// movie: 23...
