e16728f819e3cc0f4adc6aef62df38d640f1453e
Jakub Kwiatkowski <jakub1kwiatkowski@gmail.com>
2018-02-21 22:09:30 +0100

    up to 44 of 3

---
+++ script_03.js
@@ -0,0 +1,15 @@
+/**
+ * Created by jakub_000 on 2018-02-18.
+ */
+new Vue({
+    el: '#app',
+    data: {
+        show: true,
+        ingredients: ['meat', 'fruit', 'cookies'],
+        persons: [
+            {name: 'Max', age: 27, color: 'red'},
+            {name: 'Anna', age: 'unknown', color: 'blue'}
+        ]
+    }
+});
+
